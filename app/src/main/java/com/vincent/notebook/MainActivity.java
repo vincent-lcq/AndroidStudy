8013c5418fb9dc2802c08e25d7f41eb81a001d5a
vincent <vincent.li@synthesis.bz>
2026-02-04 09:28:27 +0800

    Add MainActivity

---
+++ app/src/main/java/com/vincent/notebook/MainActivity.java
@@ -0,0 +1,51 @@
+package com.vincent.notebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.vincent.notebook.adapter.NotepadListAdapter;
+import com.vincent.notebook.bean.Notepad;
+import com.vincent.notebook.databinding.ActivityMainBinding;
+import com.vincent.notebook.db.MyDbHelper;
+import com.vincent.notebook.ui.EditActivity;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private MyDbHelper myDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        myDbHelper = new MyDbHelper(this);
+        binding.addIv.setOnClickListener( view -> {
+            Intent intent = new Intent(this, EditActivity.class);
+            startActivity(intent);
+        });
+
+        NotepadListAdapter notepadListAdapter = new NotepadListAdapter();
+        binding.listRv.setAdapter(notepadListAdapter);
+//        binding.listRv.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Notepad> notepadlist = myDbHelper.findAll();
+        notepadListAdapter.update(notepadlist);
+    }
+}
